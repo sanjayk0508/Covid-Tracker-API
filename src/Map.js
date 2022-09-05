be1a4306ffa745b984388d16aed25c917fa754d8
Sanjay <102804548+sanjayk0508@users.noreply.github.com>
2022-09-05 11:44:33 +0530

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -6,6 +6,7 @@ import { showDataOnMap } from "./util";
 function Map({ countries, casesType, center, zoom }) {
   return (
     <div className="map">
+    
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -13,6 +14,7 @@ function Map({ countries, casesType, center, zoom }) {
         />
         {showDataOnMap(countries, casesType)}
       </LeafletMap>
+         
     </div>
   );
 }
